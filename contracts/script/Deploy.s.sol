871f90d2941c2b846638a4a6e0f024666edb77e8
Justice <onyekaozuru122@gmail.com>
2026-04-10 14:59:24 +0100

    script file

--- contracts/script/Deploy.s.sol
+++ contracts/script/Deploy.s.sol
@@ -1,1 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
 // Deploy script: broadcasts PaymentLink deployment to Monad Testnet
+import "forge-std/Script.sol";
+import "forge-std/console.sol";
+import "../src/PaymentLink.sol";
+
+contract Deploy is Script {
+    function run() external {
+        vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
+        PaymentLink paymentLink = new PaymentLink();
+        console.log("Deployed at:", address(paymentLink));
+        vm.stopBroadcast();
+    }
+}
